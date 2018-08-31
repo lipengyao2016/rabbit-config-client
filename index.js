78b161e64047cd3f804fdddf208db195d7ce5889
lipy <yao50cn@163.com>
2018-08-31 10:21:25 +0800

    1.添加rabbitmq客户端。

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by licp on 2016-11-28.
+ */
+
+
+
+module.exports =
+{
+    amqpLibSendClient      :    require('./lib/amqp/amqpLibSendClient'),
+    amqpLibRecvClient      :    require('./lib/amqp/amqpLibRecvClient'),
+};
